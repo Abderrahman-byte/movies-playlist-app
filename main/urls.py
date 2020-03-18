0b3911c1ec0abaaf0fed43675f9ab70964b14e29
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-03-18 17:19:34 +0100

    create login register and logout views

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegiterView.as_view(), name='register'),
+    path('logout/', views.logout, name='logout')
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 def index(request) :
     return HttpResponse('<h1>Hello World!</h1>')
+
+class LoginView(View) :
+    def get(self, request) :
+        return HttpResponse('<h1>login</h1>')
+
+class RegiterView(View) :
+    def get(self, request) :
+        return HttpResponse('<h1>Register</h1>') 
+
+def logout(request) :
+    return HttpResponse('<h1>Logged out</h1>')
