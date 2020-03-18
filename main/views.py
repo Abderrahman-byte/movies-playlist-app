3f244bcb12ff3c42da788f96480aa76690a5ee40
Abderrahman-byte <el.abderrahman00@gmail.com>
2020-03-18 21:33:42 +0100

    setup Login view

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views import View
 from django.urls import reverse
 from django.contrib import messages
+from django.contrib.auth import login, authenticate
 
 from .forms import CreateUserForm
 # Create your views here.
@@ -14,7 +15,26 @@ def index(request) :
 # LOGIN VIEW CLASS BASE
 class LoginView(View) :
     def get(self, request) :
-        return HttpResponse('<h1>login</h1>')
+        return render(request, 'main/login.html')
+
+    def post(self, request) :
+        next = request.POST.get('next')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None :
+            login(request, user)
+
+            if next is not None :
+                return redirect(next)
+            else :
+                return redirect(reverse('home'))
+        
+        else :
+            messages.error(request, 'username or password is not correct')
+            return redirect(reverse('login'))
+
 
 # REGISTER VIEW CLASS BASE
 class RegiterView(View) :
@@ -29,9 +49,11 @@ class RegiterView(View) :
 
         if form.is_valid() :
             user = form.save()
+            username = user.username
 
+            messages.success(request, f'user created {username} successfully')
             return redirect(reverse('login'))
-        
+
         else :
             for error in form.error_messages :
                 messages.error(request, form.error_messages.get(error))
